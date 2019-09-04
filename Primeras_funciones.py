f64cc7176c4cf5eba3966cfe6c0cf3978e175271
Juan-Tena <jbtena@gmail.com>
2019-09-04 16:24:03 +0200

    Sintaxis Básica III. Funciones I. Vídeo 5

---
+++ Primeras_funciones.py
@@ -0,0 +1,11 @@
+def mensaje(): #declaración de la función
+	#Cuerpo de la función
+	print("Estamos aprendiendo Python")
+	print("Estamos aprendiendo instrucciones básicas")
+	print("Poco a poco iremos avanzando")
+
+mensaje() #Llamada a la función
+
+mensaje()
+print("Ejecutando código fuera de función")
+mensaje()
