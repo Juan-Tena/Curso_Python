52e56d944916e83d6da7afa9a3f9e359dd2da9ec
Juan-Tena <jbtena@gmail.com>
2019-09-10 17:43:23 +0200

    Bucles I. For. Video 14

---
+++ Bucles determinados I. For.py
@@ -0,0 +1,15 @@
+# Bucles determinados: FOR
+# for variable in elemento a recorrer:
+#	cuerpo del bucle
+
+for i in [1,2,4]: # lista
+	print("Hola")
+
+
+for i in ["Primavera", "Verano", "Otoño", "Invierno"]:
+	print(i)
+
+print("-----------------------")
+
+for estaciones_agno in ["Primavera", "Verano", "Otoño", "Invierno"]:
+	print(estaciones_agno)
