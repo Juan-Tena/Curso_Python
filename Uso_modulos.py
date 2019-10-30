bd88c6bcf0b0ee91c4ab859cc2d3a0d8f17f3ba3
Teitum <jbtena@gmail.com>
2019-10-30 22:39:17 +0100

    Paquetes

---
+++ Uso_modulos.py
@@ -0,0 +1,12 @@
+#from calculos.calculos_generales import *
+
+#dividir(4,6)
+#potencia(2,3)
+#multiplicar(7,4)
+
+from calculos.basicos.operaciones_basicas import *
+dividir(4,6)
+multiplicar(7,4)
+
+from calculos.redondeo_potencia.redondea_potencia import *
+potencia(56,3)
---
+++ calculos/redondeo_potencia/redondea_potencia.py
@@ -0,0 +1,6 @@
+
+def potencia(base,exponente):
+	print("El resultado de elevar ", base, " a ", exponente," es: ", base**exponente)
+
+def redondear(numero):
+	print("El resultado del redondeo es: ", round(numero))
