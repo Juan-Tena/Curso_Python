d583d94034146d755f71cc0b848c0bbc333b7ae5
Teitum <jbtena@gmail.com>
2019-10-31 23:46:35 +0100

    Paquetes Distribuibles

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+	name="paquetecalculos", 
+	version="1.0",
+	descption="Paquete de redondeo y potencia",
+	author="Juan Tena",
+	author_email="jbtena@gmil.com",
+	url="www.linkedin.com/in/juantenaestelles",
+	packages=["calculos","calculos.redondeo_potencia"]
+
+
+
+	)
