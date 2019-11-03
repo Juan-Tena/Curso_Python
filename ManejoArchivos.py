3b8427d5076271630ba3f29954084411b556dc37
Teitum <jbtena@gmail.com>
2019-11-03 22:11:04 +0100

    Archivos externos II

--- ManejoArchivos.py
+++ ManejoArchivos.py
@@ -6,7 +6,7 @@ archivo_texto=open("archivo.txt", "w")
 
 frase="Estupendo día para estudiar Python \nel mércoles"
 
-archivo_texto.write(frase) #├Escribimos lo que queramos en el fichero
+archivo_texto.write(frase) #Escribimos lo que queramos en el fichero
 
 archivo_texto.close()
 
@@ -19,7 +19,7 @@ print("En el fichero pone: ", texto)
 
 archivo_texto1.close()
 
-print("----------------------------")
+print("1----------------------------")
 
 #abrimos el archivo en modo lectura
 archivo_texto2=open("archivo.txt", "r") 
@@ -32,9 +32,58 @@ archivo_texto2.close()
 print(lineas_texto[0])
 print(lineas_texto[1])
 
-print("----------------------------")
+print("2----------------------------")
 
 #abrimos el archivo en modo append, lo que nos permitirá añadir datos
 archivo_texto3=open("archivo.txt", "a")
 archivo_texto3.write("\nlo cual siempre es una buena ocasión para estudiar python")
 archivo_texto3.close()
+
+print("3----------------------------")
+
+archivo_texto3=open("archivo.txt", "r")
+print("1",archivo_texto3.read()) #Una vez lee el fichero, situa el cursor al final.
+#En esta segunda línea no lee nada porque el puntero se situa al final despues del primer read
+#   y no encuentra nada
+print("2",archivo_texto3.read())
+
+#¿Cómo podemos modificar la posición del puntero?
+archivo_texto3.seek(10)
+print("3",archivo_texto3.read())
+
+archivo_texto3.seek(0)
+print("4",archivo_texto3.read())
+
+archivo_texto3.close()
+
+#Otra manera de acceder a los datos es con read.
+#En este caso, se trata de una lectura "hasta"
+archivo_texto4=open("archivo.txt", "r")
+print("5",archivo_texto4.read(11))
+#Esta segunda lectura se realiza a partir de donde se queda el puntero
+print("6",archivo_texto4.read())
+
+archivo_texto4.close()
+
+print("4----------------------------")
+
+archivo_texto5=open("archivo.txt", "r")
+
+archivo_texto5.seek(len(archivo_texto5.read())/2)
+print(archivo_texto5.read())
+
+archivo_texto5.close()
+
+print("5----------------------------")
+
+archivo_texto6=open("archivo.txt", "r+")#Le decismos que abrimos el fichero en modo lectura y escritura
+archivo_texto6.write("Comienzo del texto")
+
+#print(archivo_texto6.readlines())
+
+lista_texto=archivo_texto6.readlines() 
+lista_texto[1]="Esta linea ha sido incluida desde el exterior \n"
+archivo_texto6.seek(0)
+archivo_texto6.writelines(lista_texto)
+
+archivo_texto6.close()
