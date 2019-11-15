218f7c1606e0753f0f3bd8bc348bcc35084cab6b
Teitum <jbtena@gmail.com>
2019-11-15 21:10:49 +0100

    Primera ventana IV

--- Graficos/Practica_label.py
+++ Graficos/Practica_label.py
@@ -12,7 +12,7 @@ miLabel.place(x=100, y=200) #Nos permite ubicar el texto el cualquier parte del 
 miImagen=PhotoImage(file="02.gif")
 Label(miFrame, image=miImagen).place(x=150, y=250)
 #Texto)
-miLabel1=Label(miFrame, text="Hola alumnos de python",fg="blue", font=("Verdana",18))
+miLabel1=Label(miFrame, text="Hola alumnos de Python",fg="blue", font=("Verdana",18))
 miLabel1.place(x=50, y=300) #Nos permite ubicar el texto el cualquier parte del frame
 
 root.mainloop()
